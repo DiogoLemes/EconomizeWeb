5c32661a0775c4206dc68a2b4e13ad96c4835885
Diogo Lemes <97925046+DiogoLemes@users.noreply.github.com>
2025-08-30 09:49:27 -0300

    base inicial do projeto
    contém 3 telas iniciais simples não finalizadas, componentes React e arquivos de configuração

---
+++ src/Logo.jsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+    return (
+        <div className="flex p-2">
+            <img src="src\assets\logo.svg" className="w-20 h-20" alt="Economize Logo"></img>
+            <p className="font-family: 'Lato', sans-serif font-medium text-logo-primary my-8 text-4xl">Economize</p>
+        </div>
+    )
+}
