bb44a8ebc1468325fca7dcab7552e0f0c7d77e85
Lucas <lucas.m.goes@unesp.br>
2025-08-30 11:33:45 -0300

    archive organization

--- src/Logo.jsx
+++
@@ -1,8 +0,0 @@
-export default function Logo() {
-    return (
-        <div className="flex p-2">
-            <img src="src\assets\logo.svg" className="w-20 h-20" alt="Economize Logo"></img>
-            <p className="font-family: 'Lato', sans-serif font-medium text-logo-primary my-8 text-4xl">Economize</p>
-        </div>
-    )
-}
