a69bbc06d94949d884a4d443436e5b2cd9fdea20
gil-a-a <gil.almeida@unesp.br>
2025-11-22 19:02:00 -0300

    swagger dos gets das transacoes

--- Api/schemas/transactions/getTransaction.js
+++ Api/schemas/transactions/getTransaction.js
@@ -1,33 +1,28 @@
 module.exports = {
-  getMonthTransactions: {
-    description: 'Rota para obter as transações do mês e ano específico. Por padrão retorna do mês atual',
+  getTransaction: {
+    description: 'Rota para obter uma transação específica de um usuário',
     tags: ['Transactions'],
-    summary: 'Obtém transações com base no ID e filtros de query',
+    summary: 'Obtém a transação com base no ID dela e do usuário',
     params: {
       type: 'object',
       properties: {
         idUsuario: { type: 'string', description: 'ID do usuario' },
+        idTrans: { type: 'string', description: 'ID da transação' },
       },
-      required: ['idUsuario'],
-    },
-    querystring: {
-      type: 'object',
-      properties: {
-        mes: { type: 'number', description: 'Mês (Opcional)' },
-        ano: { type: 'number', description: 'Ano (Opcional)' },
-      },
+      required: ['idUsuario', 'idTrans'],
     },
     response: {
       200: {
         description: 'Resposta com sucesso',
         type: 'array',
+        // additionalProperties: true,
         properties: {
           id: { type: 'number' },
           usuario_id: { type: 'number' },
           titulo: { type: 'string' },
           descricao: { type: 'string' },
           categoria_id: { type: 'number' },
-          valor: { type: 'number' },
+          valor: { type: 'string' },
           tipo: { type: 'string' },
           status_transferencia: { type: 'string' },
           criado_em: { type: 'string', format: 'date' },
